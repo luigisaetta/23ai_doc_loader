796649633f8244f46101fa4711b742e6598120c2
luigisaetta <luigi.saetta@gmail.com>
2024-09-05 19:40:17 +0200

    added template for config_private

---
+++ config_private_template.py
@@ -0,0 +1,15 @@
+"""
+Author: Luigi Saetta
+Date created: 2024-04-27
+Date last modified: 2024-04-28
+Python Version: 3.11
+"""
+
+# Moved to the shared instance
+DB_USER = "your username"
+DB_PWD = "your pwd"
+DB_HOST_IP = "your ip"
+DB_SERVICE = "your service name"
+
+# OCI (9/05) moved to new tenant
+COMPARTMENT_ID = "ocid of your compartment"
--- db_drop_collection.py
+++ db_drop_collection.py
@@ -3,13 +3,13 @@ Utility to drop an existing collection
 
 beware: check what you're doing
 """
+
 import argparse
 
 from db_doc_loader_backend import get_db_connection
 from oraclevs_4_db_loading import OracleVS4DBLoading
 
 from utils import get_console_logger
-
 
 
 #
